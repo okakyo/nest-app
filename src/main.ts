7549ba3bad6d2ea4e1e20541ce1af3ddcd64272f
Kyohei Oka <0622okakyo@gmail.com>
2021-01-26 19:19:22 +0900

    動作確認

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// 動作確認ように追加
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
