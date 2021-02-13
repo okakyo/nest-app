f59cd47e42da856ce897dac8e0b3675a881a142b
Kyohei Oka <0622okakyo@gmail.com>
2021-02-13 15:19:03 +0900

    パスの

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/apps/app.controller';
+import { AppService } from './services/apps/app.service';
+import { UsersController } from './controllers/users/users.controller';
+import { UsersService } from './services/users/users.service';
+import { UsersModule } from './modules/users/users.module';
+import { CacheModule } from './modules/cache/cache.module';
+import { CacheService } from './services/cache/cache.service';
+import { CacheController } from './controllers/cache/cache.controller';
 
 @Module({
   
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, CacheController],
+  providers: [AppService, UsersService, CacheService],
+  imports: [UsersModule, CacheModule],
 })
 export class AppModule {}
