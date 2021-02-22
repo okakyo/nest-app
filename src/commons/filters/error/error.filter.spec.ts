4709a5ec4de336b131492cfee550554942165d47
Kyohei Oka <0622okakyo@gmail.com>
2021-02-22 14:53:19 +0900

    テストバグの修正

---
+++ src/commons/filters/error/error.filter.spec.ts
@@ -0,0 +1,7 @@
+import { ErrorFilter } from './error.filter';
+
+describe('ExceptionFilter', () => {
+  it('should be defined', () => {
+    expect(new ErrorFilter()).toBeDefined();
+  });
+});
--- src/commons/filters/error/error.filter.ts
+++ src/commons/filters/error/error.filter.ts
@@ -1,6 +1,6 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 
 @Catch()
-export class ExceptionFilter<T> implements ExceptionFilter {
+export class ErrorFilter<T> implements ExceptionFilter {
   catch(exception: T, host: ArgumentsHost) {}
 }
